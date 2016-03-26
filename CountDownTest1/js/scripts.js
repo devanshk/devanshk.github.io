7c294454b5148ad39c4bd26b7cdc89eaedc840c3
dkukreja <powerdester2000@gmail.com>
2016-03-26 01:33:52 -0400

    bib

---
+++ CountDownTest1/js/scripts.js
@@ -0,0 +1,56 @@
+/*
+Count down until any date script-
+By JavaScript Kit (www.javascriptkit.com)
+Over 200+ free scripts here!
+Modified by Robert M. Kuhnhenn, D.O. 
+on 5/30/2006 to count down to a specific date AND time,
+and on 1/10/2010 to include time zone offset.
+*/
+
+/*  Change the items below to create your countdown target date and announcement once the target date and time are reached.  */
+var current="BIB X has begun!";        //—>enter what you want the script to display when the target date and time are reached, limit to 20 characters
+var year=2016;        //—>Enter the count down target date YEAR
+var month=10;          //—>Enter the count down target date MONTH
+var day=7;           //—>Enter the count down target date DAY
+var hour=6;          //—>Enter the count down target date HOUR (24 hour clock)
+var minute=0;        //—>Enter the count down target date MINUTE
+var tz=-5;            //—>Offset for your timezone in hours from UTC (see http://wwp.greenwichmeantime.com/index.htm to find the timezone offset for your location)
+
+//—>    DO NOT CHANGE THE CODE BELOW!    <—
+var montharray=new Array("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec");
+
+function countdown(yr,m,d,hr,min){
+theyear=yr;themonth=m;theday=d;thehour=hr;theminute=min;
+var today=new Date();
+var todayy=today.getYear();
+if (todayy < 1000) {
+todayy+=1900; }
+var todaym=today.getMonth();
+var todayd=today.getDate();
+var todayh=today.getHours();
+var todaymin=today.getMinutes();
+var todaysec=today.getSeconds();
+var todaystring1=montharray[todaym]+" "+todayd+", "+todayy+" "+todayh+":"+todaymin+":"+todaysec;
+var todaystring=Date.parse(todaystring1)+(tz*1000*60*60);
+var futurestring1=(montharray[m-1]+" "+d+", "+yr+" "+hr+":"+min);
+var futurestring=Date.parse(futurestring1)-(today.getTimezoneOffset()*(1000*60));
+var dd=futurestring-todaystring;
+var dday=Math.floor(dd/(60*60*1000*24)*1);
+var dhour=Math.floor((dd%(60*60*1000*24))/(60*60*1000)*1);
+var dmin=Math.floor(((dd%(60*60*1000*24))%(60*60*1000))/(60*1000)*1);
+var dsec=Math.floor((((dd%(60*60*1000*24))%(60*60*1000))%(60*1000))/1000*1);
+if(dday<=0&&dhour<=0&&dmin<=0&&dsec<=0){
+    $("#days").html(0);
+    $("#hours").html(0);
+    $("#minutes").html(0);
+    $("#seconds").html(0);
+    return;
+}
+else {
+    $("#days").html(dday);
+    $("#hours").html(dhour);
+    $("#minutes").html(dmin);
+    $("#seconds").html(dsec);
+    setTimeout("countdown(theyear,themonth,theday,thehour,theminute)",1000);
+    }
+}
